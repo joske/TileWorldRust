78a7eaebb14fd85135b476f472ce38da0b6a9a8c
Jos Dehaes <jos.dehaes@gmail.com>
2023-04-08 23:13:34 +0200

    no need to take reference

--- src/astar.rs
+++ src/astar.rs
@@ -69,16 +69,14 @@ pub(crate) fn astar(grid: &Grid, from: Location, to: Location) -> Option<Vec<Dir
             Direction::Down,
             Direction::Left,
             Direction::Right,
-        ]
-        .iter()
-        {
-            if cur_location.is_valid_move(*d) {
-                let next_location = cur_location.next_location(*d);
+        ] {
+            if cur_location.is_valid_move(d) {
+                let next_location = cur_location.next_location(d);
                 if next_location == to || grid.is_free(&next_location) {
                     let h = next_location.distance(to);
                     let g = cur_node.path.len() as u16 + 1;
                     let mut new_path = cur_node.path.clone();
-                    new_path.push(*d);
+                    new_path.push(d);
                     let child = Node::new(next_location, g + h, new_path);
                     if !closed_list.contains(&next_location)
                         && !open_list
